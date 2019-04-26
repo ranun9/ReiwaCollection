870cfa261b96a49e564078f03d6c118d9707d40e
ranun9 <ranun9@example.com>
2019-04-26 16:52:05 +0900

    Firebase C++ SDK　連携

--- proj.android/app/jni/Android.mk
+++ proj.android/app/jni/Android.mk
@@ -1,5 +1,24 @@
 LOCAL_PATH := $(call my-dir)
 
+# The path to the Firebase C++ SDK, in the project's root directory.
+FIREBASE_CPP_SDK_DIR := ../../../firebase_cpp_sdk
+
+APP_ABI := armeabi-v7a x86
+STL := $(firstword $(subst _, ,$(APP_STL)))
+FIREBASE_LIBRARY_PATH := $(FIREBASE_CPP_SDK_DIR)/libs/android/$(TARGET_ARCH_ABI)/$(STL)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := firebase_app
+LOCAL_SRC_FILES := $(FIREBASE_LIBRARY_PATH)/libfirebase_app.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(FIREBASE_CPP_SDK_DIR)/include
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := firebase_feature
+LOCAL_SRC_FILES := $(FIREBASE_LIBRARY_PATH)/libfirebase_admob.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(FIREBASE_CPP_SDK_DIR)/include
+include $(PREBUILT_STATIC_LIBRARY)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := MyGame_shared
@@ -19,6 +38,8 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes
 
 
 LOCAL_STATIC_LIBRARIES := cc_static
+LOCAL_STATIC_LIBRARIES += firebase_app
+LOCAL_STATIC_LIBRARIES += firebase_feature
 
 # _COCOS_LIB_ANDROID_BEGIN
 # _COCOS_LIB_ANDROID_END
